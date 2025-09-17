6666849623fc065fc78f456b1adff3f901a99a7c
csandahl <sandahlcharlie@gmail.com>
2025-09-16 22:10:37 -0500

    I finished the ClothingArrayDriver and almost finished the main program, made a backup of garments.txt because one of the methods adds an int each time it is run

--- src/ClothingArrayDriver.java
+++ src/ClothingArrayDriver.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+
 public class ClothingArrayDriver {
+    public static int overallAvg(Clothing[] a){
+        int sum = 0;
+        int count = 0;
+        for(Clothing item: a) {
+            sum += (item).getStock();
+            count++;
+        }
+        return sum/count;
+    }
+
+    public static int count(Clothing[] a, Clothing o){
+        int count = 0;
+        for(Clothing item: a){
+            if(item.equals(o)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int groupAvg(Clothing[] a, String s){
+       int sum = 0;
+       int count = 0;
+       for(Clothing item: a){
+           if(item.getGarment().equals(s)) {
+                sum += (item).getStock();
+                count++;
+           }
+       }
+        return sum/count;
+    }
+
+    public Clothing[] lessThan(Clothing[] a, Integer i){
+        ArrayList<Clothing> less = new ArrayList<>();
+        for(Clothing item: a){
+            if(item.getStock() < i){
+                less.add(item);
+            }
+        }
+        return less.toArray(new Clothing[0]);
+    }
+
+    public void groupEdit(Clothing[] a, String s, Integer n){
+        for(Clothing item: a){
+            if(item.getGarment().equals(s)) {
+                item.setStock(item.getStock()+n);
+            }
+        }
+    }
 }
--- src/main.java
+++ src/main.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 
 public static void main(String[] args) {
     // Make the clothing array
-    ArrayList<Clothing> ClothingArr = new ArrayList<>();
+    ArrayList<Clothing> inventory = new ArrayList<>();
 
     // Read input from a user for a file
     Scanner input = new Scanner(System.in);
@@ -20,14 +20,21 @@ public static void main(String[] args) {
         while(fileScanner.hasNext()){
             String garment = fileScanner.next();
             int stock = fileScanner.nextInt();
-            ClothingArr.add(new Clothing(garment, stock));
+            inventory.add(new Clothing(garment, stock));
         }
     } catch(FileNotFoundException e){
         System.out.println("Error reading the file: " + e.getMessage());
     }
-    for(Clothing garment: ClothingArr){
+    for(Clothing garment: inventory){
        System.out.println(garment);
     }
+    Clothing[] ClothingArr = inventory.toArray(new Clothing[0]);
 
-
+    // Testing methods for Array Driver
+    System.out.println("We have an average of " + ClothingArrayDriver.overallAvg(ClothingArr) + " in stock across our " +
+            "garment collection");
+    System.out.println("We have " + ClothingArrayDriver.count(ClothingArr, new Clothing("tshirt", 9)) +
+            " T-shirts that have 9 in stock");
+    System.out.println("We have an average of " + ClothingArrayDriver.groupAvg(ClothingArr, "tshirt") + " in stock");
+    System.out.println();
 }
