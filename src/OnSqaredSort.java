5157778c76105e6e9bc14a8d431af0813301be54
sandahlcharlie <sandahlcharlie@gmail.com>
2025-11-14 12:20:39 -0600

    Added all the class files for next assignment and started to write the Sort class.

---
+++ src/OnSqaredSort.java
@@ -0,0 +1,2 @@
+public class OnSqaredSort {
+}
---
+++ src/Sort.java
@@ -0,0 +1,9 @@
+public abstract class Sort {
+    protected int[][] intitial;
+    public static final int TEST = 20;
+    public Stopwatch sw;
+
+    public Sort(){
+
+    }
+}
