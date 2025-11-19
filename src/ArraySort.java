806b7d1b13f1f2723f6568fe9938f411defb2129
sandahlcharlie <sandahlcharlie@gmail.com>
2025-11-18 19:44:53 -0600

    Added Array Sort with stopwatch

---
+++ src/ArraySort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class ArraySort extends Sort {
+    public ArraySort() {
+        super();
+        int[][] sort = new int[initial.length][];
+
+        for (int i = 0; i < initial.length; i++) {
+            sort[i] = new int[initial[i].length];
+            System.arraycopy(initial[i], 0, sort[i], 0, initial[i].length);
+        }
+
+        for (int i = 0; i < sort.length; i++) {
+            sw.startTimer();
+            Arrays.sort(sort[i]);
+            sw.stopTimer();
+
+            if (i == 0) {
+                // Print the test array (row 0)
+                System.out.println("ArraysSort - TEST array:");
+                System.out.println("Before sorting:");
+                show(initial[0]);
+                System.out.println("\nAfter sorting:");
+                show(sort[0]);
+                System.out.println("Is it sorted? " + isSorted(sort[0]));
+            } else {
+                System.out.println("ArraysSort - Array " + i + " (size " + sort[i].length +
+                        "): " + sw.elapsedTime() + " ms");
+            }
+        }
+    }
+}
