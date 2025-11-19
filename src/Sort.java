4bec3b6c83d0d3e020c6eb0dca224ac519486ba7
sandahlcharlie <sandahlcharlie@gmail.com>
2025-11-18 19:27:24 -0600

    Made the sort and stopwatch classes

--- src/Sort.java
+++ src/Sort.java
@@ -1,9 +1,38 @@
+import java.util.Random;
+
 public abstract class Sort {
-    protected int[][] intitial;
+    protected int[][] initial;
     public static final int TEST = 20;
     public Stopwatch sw;
 
-    public Sort(){
+    public Sort() {
+        int[] sizes = {TEST, 1000, 2000, 3000, 4000, 5000};
+        initial = new int[sizes.length][];
+        Random r = new Random();
 
+        for (int i = 0; i < sizes.length; i++) {
+            initial[i] = new int[sizes[i]];
+            for (int j = 0; j < sizes[i]; j++) {
+                initial[i][j] = r.nextInt();
+            }
+        }
+        sw = new Stopwatch();
+
+    }
+
+    public static int show(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%20d%n", a[i]);
+        }
+        return a.length;
+    }
+
+    public boolean isSorted(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,2 +1,20 @@
 public class Stopwatch {
+    private long startTime;
+    private long stopTime;
+    public Stopwatch() {
+        startTime = 0;
+        stopTime = 0;
+    }
+
+    public void startTimer() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stopTimer() {
+        stopTime = System.currentTimeMillis();
+    }
+
+    public long elapsedTime() {
+        return stopTime - startTime;
+    }
 }
