98da6f3ea95d2d41ae0d196e5ff89337493f859e
sandahlcharlie <sandahlcharlie@gmail.com>
2025-11-18 20:37:03 -0600

    Added bubble sort (On^2) algo from geeksforgeeks

--- src/OnSqaredSort.java
+++ src/OnSqaredSort.java
@@ -1,3 +1,46 @@
-public class OnSqaredSort {
 
+// Bubble Sort algorithm adapted from https://www.geeksforgeeks.org/dsa/bubble-sort-algorithm/
+// removed the swapped flag to make it closer to On^2
+public class OnSqaredSort extends Sort {
+    public OnSqaredSort(){
+        super();
+        int[][] sort = new int[initial.length][];
+
+        for (int i = 0; i < initial.length; i++) {
+            sort[i] = new int[initial[i].length];
+            System.arraycopy(initial[i], 0, sort[i], 0, initial[i].length);
+        }
+        for (int i = 0; i < sort.length; i++) {
+            sw.startTimer();
+            bubbleSort(sort[i]);
+            sw.stopTimer();
+
+            if (i == 0) {
+                // Print the test array (row 0)
+                System.out.println("\nBubbleSort - TEST array");
+                System.out.println("Before sorting:");
+                show(initial[0]);
+                System.out.println("\nAfter sorting:");
+                show(sort[0]);
+                System.out.println("Is sorted? " + isSorted(sort[0]));
+            } else {
+                System.out.println("BubbleSort - Array " + i + " (size " + sort[i].length +
+                        "): " + sw.elapsedTime() + " ms");
+            }
+        }
+
+    }
+    private void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int i, j, temp;
+        for (i = 0; i < 1; i++) {
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
